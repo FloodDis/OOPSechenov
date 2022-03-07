8b4074a2aabb8d94f5c7ed9b086e27b5a456f86f
FloodDis <vlad.sechenov@mail.ru>
2022-03-07 19:14:34 +0700

    refactoring(2.2.2.1.cpp): исправил значение поля Genre

--- Exceptions/2.2.2.1.cpp
+++ Exceptions/2.2.2.1.cpp
@@ -17,8 +17,8 @@ void DemoStructures1()
 	testFlight.FlightTime = 60;
 
 	testFilm.Duration = 120;
-	// TODO: комедия?
-	testFilm.Genre = "Comedy";
+	// TODO: комедия? +
+	testFilm.Genre = "Horror";
 	testFilm.Name = "Scream";
 	testFilm.YearOfRelease = 2001;
 
