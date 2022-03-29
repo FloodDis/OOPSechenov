73bef4386ab593de8792b3c7bed458e55c1a8d3c
FloodDis <vlad.sechenov@mail.ru>
2022-03-29 15:34:57 +0700

    Сделал задания 5.1.5 и 5.1.6

--- LAB5/Resource Files/Main.cpp
+++ LAB5/Resource Files/Main.cpp
@@ -1,9 +1,27 @@
 #include "../Header Files/Inheritance/Teacher.h"
+#include "../Header Files/Inheritance/DemoInheritence.h"
+#include "../Header Files/Inheritance/Student.h"
 #include <iostream>
 using namespace std;
 
 void main()
 {
 	setlocale(LC_ALL, "ru");
+	Person* person = new Person("Владислав", "Сеченов", "Вадимович");
+	ShowName(person);
+	delete person;
 
+	Student* student = new Student();
+	student->SetName("Владислав");
+	student->SetSurname("Сеченов");
+	student->SetMiddleName("Вадимович");
+	ShowName(student);
+	delete student;
+
+	Teacher* teacher = new Teacher();
+	teacher->SetName("Владислав");
+	teacher->SetSurname("Сеченов");
+	teacher->SetMiddleName("Вадимович");
+	ShowName(teacher);
+	delete teacher;
 }
